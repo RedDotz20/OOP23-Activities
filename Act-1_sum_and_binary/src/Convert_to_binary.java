25bab07aa0491bb09c5c39864e06a0eb62279451
RedDotz20 <carlos_tabangay@outlook.com>
2023-01-31 10:15:26 +0800

    first activity initialization

---
+++ Act-1_sum_and_binary/src/Convert_to_binary.java
@@ -0,0 +1,5 @@
+public class Convert_to_binary {
+    public static void main(String[] args) {
+        System.out.println("convert to binary");
+    }
+}
---
+++ Act-1_sum_and_binary/src/Return_sum.java
@@ -0,0 +1,5 @@
+public class Return_sum {
+    public static void main(String[] args) {
+        System.out.println("return sum");
+    }
+}
