0008e5751aff5fa1f060e99946d688be994a0bcd
RedDotz20 <carlos_tabangay@outlook.com>
2023-01-31 11:14:33 +0800

    Math_operations finished

---
+++ Act-1_sum_and_binary/src/Math_operations.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Math_operations {
+    public static void operations(int firstNum, int secondNum) {
+       int add = firstNum + secondNum,
+               subtract =  firstNum - secondNum,
+               multiply = firstNum * secondNum,
+               divide = firstNum / secondNum,
+               modulo = firstNum % secondNum;
+
+       System.out.println(
+               firstNum + " + " + secondNum + " = " + add       +'\n'+
+               firstNum + " - " + secondNum + " = " + subtract  +'\n'+
+               firstNum + " * " + secondNum + " = " + multiply  +'\n'+
+               firstNum + " / " + secondNum + " = " + divide    +'\n'+
+               firstNum + " mod " + secondNum + " = " + modulo
+       );
+    }
+
+    public static void main(String[] args) {
+        Scanner values = new Scanner(System.in);
+
+        // Get First Input Value (integer)
+        System.out.print("Enter The First Value: ");
+        int firstInputValue = values.nextInt();
+
+        // Get Second Input Value (integer)
+        System.out.print("Enter The Second Value: ");
+        int secondInputValue = values.nextInt();
+
+        // Display / Print output (operations method)
+        operations(firstInputValue, secondInputValue);
+    }
+}
--- Act-1_sum_and_binary/src/Return_sum.java
+++
@@ -1,5 +0,0 @@
-public class Return_sum {
-    public static void main(String[] args) {
-        System.out.println("return sum");
-    }
-}
